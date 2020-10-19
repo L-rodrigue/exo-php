8ba630487d177a48e7b816496cf6f6593a2a02cf
Lepez_Rodrigue <lepezrodrigue1@gmail.com>
2020-10-20 01:08:58 +0200

    add script php

---
+++ demo.php
@@ -0,0 +1,9 @@
+<?php
+$nom ='Doe';
+$prenom = 'Marc';
+$note1 = 10;
+$note2 = 20;
+$resulta = (($note1 + $note2)/2);
+
+echo 'Bonjour'.$prenom.' '.$nom.' '.'vous avez eu'.' '.$resulta.' '.'de moyenne';
+?>
